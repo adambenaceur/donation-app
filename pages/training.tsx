0eef3d8e0a3029f6706c05d2b99a30e977bbc984
adambenaceur <benaceur.adam@gmail.com>
2022-05-31 12:04:21 -0600

    added a training page

---
+++ pages/training.tsx
@@ -0,0 +1,17 @@
+import type { NextPage } from 'next'
+import Header from '../components/Header'
+
+
+const Training: NextPage = () => {
+
+  return (
+
+      <div >
+        <Header/>
+        <h1>training!</h1>
+      </div>
+
+  )
+}
+
+export default Training
