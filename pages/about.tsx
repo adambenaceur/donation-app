f0063abae7e8304e668b0cbbaa8e7caca8431805
adambenaceur <benaceur.adam@gmail.com>
2022-05-31 12:03:30 -0600

    added about page

---
+++ pages/about.tsx
@@ -0,0 +1,17 @@
+import type { NextPage } from 'next'
+import Header from '../components/Header'
+
+
+const About: NextPage = () => {
+
+  return (
+
+      <div >
+        <Header/>
+        <h1>about!</h1>
+      </div>
+
+  )
+}
+
+export default About
