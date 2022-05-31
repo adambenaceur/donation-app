429f2426d40d910d38a30b0a680cd5897dc7f039
adambenaceur <benaceur.adam@gmail.com>
2022-05-31 12:02:26 -0600

    added a contact page

---
+++ pages/contact.tsx
@@ -0,0 +1,17 @@
+import type { NextPage } from 'next'
+import Header from '../components/Header'
+
+
+const Contact: NextPage = () => {
+
+  return (
+
+      <div >
+        <Header/>
+        <h1>contact!</h1>
+      </div>
+
+  )
+}
+
+export default Contact
